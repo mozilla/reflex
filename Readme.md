cd260002072a3a6b19e1f862342f63c71d9810b6
Irakli Gozalishvili <rfobic@gmail.com>
2012-10-20 15:51:48 -0700

    Initial version.

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# repipe
+
+[![Build Status](https://secure.travis-ci.org/Gozala/repipe.png)](http://travis-ci.org/Gozala/repipe)
+
+Functional reactive UI library
+
+## Install
+
+    npm install repipe
---
+++ repipe.js
@@ -0,0 +1,6 @@
+/* vim:set ts=2 sw=2 sts=2 expandtab */
+/*jshint asi: true undef: true es5: true node: true browser: true devel: true
+         forin: true latedef: false globalstrict: true*/
+
+"use strict";
+
