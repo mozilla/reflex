fdcc172bd36425c46fd756dc7a35bee6b3c2dee4
Irakli Gozalishvili <rfobic@gmail.com>
2012-10-28 20:51:18 -0700

    Implement necessary utility functions.

---
+++ util/isnt.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function isnt(expected) {
+  /**
+  High order function that returns an assertion function which compares
+  with curried `expected` against passed value via `===` and returns the
+  result.
+  **/
+  function assert(actual) {
+    return actual !== expected
+  }
+}
+
+module.exports = isnt
