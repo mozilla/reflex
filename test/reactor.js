52dd22d3a1beb436597a2716e5421c13378dd7fb
Irakli Gozalishvili <rfobic@gmail.com>
2012-11-06 14:15:14 -0800

    Add some reactor tests.

---
+++ test/reactor.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var test = require("reducers/test/util/test")
+
+var writer = require("../writer")
+var reactor = require("../reactor")
+
+var concat = require("reducers/concat")
+var delay = require("reducers/delay")
+
+
+var write = writer(function swap(output, delta) {
+  output.push(delta)
+}, function close(output) {
+  output.push(null)
+}, function open(options) {
+  return []
+})
+
+exports["test reactor"] = test(function(assert) {
+  var r = reactor(write, function(output) {
+    // Delay output so that writer will have a chance to accumulate inputs.
+    return concat(delay("<="), output)
+  })
+  var actual = r([1, 2, 3])
+
+  assert(actual, ["<=", 1, 2, 3, null], "reactor reads and writes")
+})
+
+if (require.main === module)
+  require("test").run(exports)
