e4c1b21ec15e03d41e7cd10e565fb6f5e118d07a
Irakli Gozalishvili <rfobic@gmail.com>
2012-11-07 13:40:23 -0800

    Make drag writer a reactor example.

--- examples/drag-writer/drag-writer.js
+++ examples/drag-writer/drag-writer.js
@@ -6,37 +6,43 @@ var open = require("dom-reduce/event")
 var merge = require("reducers/flatten")
 var takeWhile = require("reducers/take-while")
 var expand = require("reducers/expand")
+var reactor = require("reflex/reactor")
+var print = require("reducers/debug/print")
 
-// Get a signal of mouse down events.
-var mousedown = open(document.documentElement, "mousedown")
+function dragReader(view) {
+  // Get a signal of mouse down events.
+  var mousedown = open(view, "mousedown")
 
-// And signals for mouse up and move events.
-var mouseup = open(document.documentElement, "mouseup")
-var mousemove = open(document.documentElement, "mousemove")
+  // And signals for mouse up and move events.
+  var mouseup = open(view, "mouseup")
+  var mousemove = open(view, "mousemove")
 
-// Relocation signal can be expressed as events from mixed **move** & **up**
-// events until firs **up** occurs.
-var relocate = takeWhile(merge([ mousemove, mouseup ]), function(event) {
-  return event.type !== "mouseup"
-})
-
-
-// Item drag signal can be expressed as expansion of mousedown events
-// to a followup relocation events.
-var drag = expand(mousedown, function(event) {
-  // Calculate offset.
-  var offsetX = event.offsetX || event.layerX
-  var offsetY = event.offsetY || event.layerY
-
-  // Map relocation events to an axis with offset being applied.
-  return map(relocate, function(event) {
-    return { x: event.clientX - offsetX, y: event.clientY - offsetY }
+  // Relocation signal can be expressed as events from mixed **move** & **up**
+  // events until firs **up** occurs.
+  var relocate = takeWhile(merge([ mousemove, mouseup ]), function(event) {
+    return event.type !== "mouseup"
   })
-})
+
+
+    // Item drag signal can be expressed as expansion of mousedown events
+    // to a followup relocation events.
+  var drag = expand(mousedown, function(event) {
+    // Calculate offset.
+    var offsetX = event.offsetX || event.layerX
+    var offsetY = event.offsetY || event.layerY
+
+    // Map relocation events to an axis with offset being applied.
+    return map(relocate, function(event) {
+      return { x: event.clientX - offsetX, y: event.clientY - offsetY }
+    })
+  })
+
+  return drag
+}
 
 // Create a writer for a our box element which will
 // apply drag axis to it.
-var box = writer(function swap(view, state) {
+var positionWriter = writer(function swap(view, state) {
   view.style.left = state.x + "px"
   view.style.top = state.y + "px"
 }, function close(view) {
@@ -44,5 +50,6 @@ var box = writer(function swap(view, state) {
   return document.getElementById("box")
 })
 
-// Write drag states into box
-box(drag)
+
+var draggable = reactor(positionWriter, dragReader)
+draggable()
