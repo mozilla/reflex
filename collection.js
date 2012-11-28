6a4645878d6d3f27cd54331b005282bb171b73c7
Irakli Gozalishvili <rfobic@gmail.com>
2012-11-28 01:34:19 -0800

    Move DOM related things out of the collection implementation.

--- collection.js
+++ collection.js
@@ -15,26 +15,18 @@ var has = require("oops/has")
 var field = require("oops/field")
 var isnt = require("oops/isnt")
 
-function collection(write, name) {
-  return function(input, target) {
-    var template = target.querySelector("[data-field='" + name + "']")
-    var root = template.parentNode
-    root.innerHTML = ""
-
+function collection(writer) {
+  return function(input, target, context) {
+    context = context || []
     input = hub(input)
+    var write = writer(input, target, context)
     fold(input, function(delta, state) {
       Object.keys(delta).map(function(id) {
         if (state[id] === void(0)) {
-          var view = template.cloneNode(true)
           var fork = concat(delta, input)
           var changes = filter(fork, has(id))
           var updates = map(changes, field(id))
-          write(takeWhile(updates, isnt(null)), view)
-
-          // TODO: Instead of just appending `view` there should
-          // be a function that decides whether to append / prepend
-          // `view` or maybe even rearrange them to do sorting.
-          root.appendChild(view)
+          write(takeWhile(updates, isnt(null)), target, context.concat(id))
         }
       })
       return patch(state, delta)
