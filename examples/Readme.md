c55ed926f61f399b2aadd941803efb251756b47c
Irakli Gozalishvili <rfobic@gmail.com>
2014-09-20 01:08:25 +0400

    Remove old examples.

--- examples/Readme.md
+++
@@ -1,4 +0,0 @@
-# Examples
-
-Index of examples that demonstrate role and use of individual building block
-provided by the reflex library.
--- examples/widgets/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var reducers = require("reducers")
-var dom = require("dom-reduce")
-var widget = require("./widget")
-
-var interactive = require("interactivate/main")
