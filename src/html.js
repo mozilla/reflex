e7e57b804383aefacadfda1c9cc51e4de8512109
gozala <rfobic@gmail.com>
2016-04-19 11:11:13 -0700

    Enable type checker for `html.js` file.

--- src/html.js
+++ src/html.js
@@ -1,28 +1,35 @@
-/* @noflow */
+/* @flow */
 
 import {node} from "./dom"
 /*::
 import type {Html, element} from "./html"
 */
 
-export const html/*:Html*/ = Object.create(null);
 
-["a","abbr","address","area","article","aside","audio","b","base","bdi",
- "bdo","big","blockquote","body","br","button","canvas","caption","cite",
- "code","col","colgroup","data","datalist","dd","del","details","dfn",
- "dialog","div","dl","dt","em","embed","fieldset","figcaption","figure",
- "footer","form","h1","h2","h3","h4","h5","h6","head","header","hr","html",
- "i","iframe","img","input","ins","kbd","keygen","label","legend","li","link",
- "main","map","mark","menu","menuitem","meta","meter","nav","noscript",
- "object","ol","optgroup","option","output","p","param","picture","pre",
- "progress","q","rp","rt","ruby","s","samp","script","section","select",
- "small","source","span","strong","style","sub","summary","sup","table",
- "tbody","td","textarea","tfoot","th","thead","time","title","tr","track",
- "u","ul","var","video","wbr","circle","clipPath","defs","ellipse","g","line",
- "linearGradient","mask","path","pattern","polygon","polyline","radialGradient",
- "rect","stop","svg","text","tspan"].forEach(tagName => {
-    const element/*:element*/ = (properties, children) =>
-      node(tagName, properties, children)
+export const html/*:Html*/ =
+  ["a","abbr","address","area","article","aside","audio","b","base","bdi",
+   "bdo","big","blockquote","body","br","button","canvas","caption","cite",
+   "code","col","colgroup","data","datalist","dd","del","details","dfn",
+   "dialog","div","dl","dt","em","embed","fieldset","figcaption","figure",
+   "footer","form","h1","h2","h3","h4","h5","h6","head","header","hr","html",
+   "i","iframe","img","input","ins","kbd","keygen","label","legend","li","link",
+   "main","map","mark","menu","menuitem","meta","meter","nav","noscript",
+   "object","ol","optgroup","option","output","p","param","picture","pre",
+   "progress","q","rp","rt","ruby","s","samp","script","section","select",
+   "small","source","span","strong","style","sub","summary","sup","table",
+   "tbody","td","textarea","tfoot","th","thead","time","title","tr","track",
+   "u","ul","var","video","wbr","circle","clipPath","defs","ellipse","g","line",
+   "linearGradient","mask","path","pattern","polygon","polyline","radialGradient",
+   "rect","stop","svg","text","tspan"
+  ]
+  .reduce
+  ( (html, tagName) => {
+      const element/*:element*/ = (properties, children) =>
+        node(tagName, properties, children)
 
-    html[tagName] = element
-});
+      html[tagName] = element
+      return html
+    }
+    // @Hack: Workarounding facebook/flow#1683
+  , (Object.create(null)/*::, {}*/)
+  );
