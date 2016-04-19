30d7a4d964548ab7fce36db83add1143da938d68
gozala <rfobic@gmail.com>
2016-04-19 11:17:09 -0700

    Reduce number of allocations in task.js
    Task schedulers stack unwinding code was written in pure functional style & performed many allocations. This patch rewrites that code to avoid allocations when processing a task.

--- src/task.js
+++ src/task.js
@@ -1,22 +1,26 @@
 /* @flow */
 
+
 /*::
-import type {Time, ThreadID} from "./task"
 import type {Address} from "./signal"
 
-export type {ThreadID, Time}
+export type ProcessID = number
+export type ThreadID = number
+export type Time = number
+
+type Abort <reason> =
+  (reason:reason) => void;
 */
 
 export class Task /*::<x, a>*/ {
-  static create /*::<x, a>*/(execute/*:(succeed:(a:a) => void, fail:(x:x) => void) => void*/)/*:Task<x, a>*/ {
+  static create /*::<x, a>*/(execute/*:(succeed:(a:a) => void, fail:(x:x) => void) => ?Abort<any>*/)/*:Task<x, a>*/ {
+    console.warn('Task.create is deprecated API use new Task instead')
     return new Task(execute)
   }
-
   static future /*::<x, a>*/(request/*:() => Promise<a>*/)/*:Task<x, a>*/ {
-    console.warn('Task.future is deprecated API use Task.create instead')
+    console.warn('Task.future is deprecated API use new Task instead')
     return new Future(request)
   }
-
   static succeed /*::<x, a>*/(value/*:a*/)/*:Task<x, a>*/ {
     return new Succeed(value)
   }
@@ -29,7 +33,6 @@ export class Task /*::<x, a>*/ {
     return new Spawn(task)
   }
 
-
   static sleep /*::<x>*/ (time:Time)/*:Task<x, void>*/ {
     return new Sleep(time)
   }
@@ -38,14 +41,14 @@ export class Task /*::<x, a>*/ {
     return new Send(address, message)
   }
 
-  static fork /*::<x, a>*/(task/*:Task<x, a>*/, onSucceed/*:(a:a) => void*/, onFail/*:(x:x) => void*/)/*:void*/ {
-    run(new Running(task), onSucceed, onFail)
+  static fork /*::<x, a, message, reason>*/(task/*:Task<x, a>*/, onSucceed/*:(a:a) => void*/, onFail/*:(x:x) => void*/)/*:Process<message, reason>*/ {
+    return Process.fork(task, onSucceed, onFail)
   }
 
   /*::
-  execute: (succeed:(a:a) => void, fail:(x:x) => void) => void;
+  execute: (succeed:(a:a) => void, fail:(x:x) => void) => ?Abort<any>;
   */
-  constructor(execute/*:(succeed:(a:a) => void, fail:(x:x) => void) => void*/) {
+  constructor(execute/*:(succeed:(a:a) => void, fail:(x:x) => void) => ?Abort<any>*/) {
     this.execute = execute
   }
   chain /*::<b>*/(next/*:(a:a) => Task<x,b>*/)/*:Task<x, b>*/ {
@@ -68,46 +71,48 @@ export class Task /*::<x, a>*/ {
   }
 }
 
-
-
 class Succeed /*::<x,a>*/ extends Task /*::<x, a>*/ {
   /*::
   value: a;
   */
   constructor(value/*:a*/) {
-    super(Succeed.prototype.fork)
+    super(Succeed$prototype$fork)
     this.value = value
   }
   fork(succeed/*:(a:a) => void*/, fail/*:(x:x) => void*/)/*:void*/ {
     succeed(this.value)
   }
 }
+const Succeed$prototype$fork = Succeed.prototype.fork
 
 class Fail /*::<x, a>*/ extends Task /*::<x, a>*/ {
   /*::
   error: x;
   */
   constructor(error/*:x*/) {
-    super(Fail.prototype.fork)
+    super(Fail$prototype$fork)
     this.error = error
   }
   fork(succeed/*:(a:a) => void*/, fail/*:(x:x) => void*/)/*:void*/ {
     fail(this.error)
   }
 }
+const Fail$prototype$fork = Fail.prototype.fork
 
 class Sleep /*::<x, a:void>*/ extends Task /*::<x, void>*/ {
   /*::
   time: Time;
   */
   constructor(time/*:Time*/) {
-    super(Sleep.prototype.fork)
+    super(Sleep$prototype$execute)
     this.time = time
   }
-  fork(succeed/*:(a:a) => void*/, fail/*:(x:x) => void*/)/*:void*/ {
-    setTimeout(succeed, this.time, void(0))
+  execute(succeed/*:(a:a) => void*/, fail/*:(x:x) => void*/)/*:?Abort<void>*/ {
+    const id = setTimeout(succeed, this.time, void(0))
+    return () => clearTimeout(id)
   }
 }
+const Sleep$prototype$execute = Sleep.prototype.execute
 
 let threadID = 0
 class Spawn /*::<x, y, a>*/ extends Task /*::<y, ThreadID>*/ {
@@ -115,7 +120,7 @@ class Spawn /*::<x, y, a>*/ extends Task /*::<y, ThreadID>*/ {
   task: Task<x, a>;
   */
   constructor(task/*:Task<x, a>*/) {
-    super(Spawn.prototype.fork)
+    super(Spawn$prototype$fork)
     this.task = task
   }
   fork(succeed/*:(a:ThreadID) => void*/, fail/*:(x:y) => void*/)/*:void*/ {
@@ -126,6 +131,7 @@ class Spawn /*::<x, y, a>*/ extends Task /*::<y, ThreadID>*/ {
     succeed(++threadID)
   }
 }
+const Spawn$prototype$fork = Spawn.prototype.fork
 
 class Send /*::<x, a>*/ extends Task /*::<x, void>*/ {
   /*::
@@ -133,7 +139,7 @@ class Send /*::<x, a>*/ extends Task /*::<x, void>*/ {
   address: Address<a>;
   */
   constructor(address/*:Address<a>*/, message/*:a*/) {
-    super(Send.prototype.fork)
+    super(Send$prototype$fork)
     this.message = message
     this.address = address
   }
@@ -141,55 +147,57 @@ class Send /*::<x, a>*/ extends Task /*::<x, void>*/ {
     succeed(void(this.address(this.message)))
   }
 }
+const Send$prototype$fork = Send.prototype.fork
 
 class Future /*::<x, a>*/ extends Task/*::<x, a>*/ {
   /*::
   request: () => Promise<a>;
   */
   constructor(request/*:() => Promise<a>*/) {
-    super(Future.prototype.fork)
+    super(Future$prototype$fork)
     this.request = request
   }
   fork(succeed/*:(a:a) => void*/, fail/*:(x:x) => void*/)/*:void*/ {
     this.request().then(succeed, fail)
   }
 }
+const Future$prototype$fork = Future.prototype.fork
 
-class Chain /*::<x, a, b>*/ extends Task/*::<x, a>*/ {
+class Chain /*::<x, a, b>*/ extends Task/*::<x, b>*/ {
   /*::
-  task: Task<x, b>;
-  next: (input:b) => Task<x, a>;
+  task: Task<x, a>;
+  next: (input:a) => Task<x, b>;
   */
-  constructor(task/*:Task<x, b>*/, next/*:(input:b) => Task<x, a>*/) {
-    super(Chain.prototype.fork)
+  constructor(task/*:Task<x, a>*/, next/*:(input:a) => Task<x, b>*/) {
+    super(Chain$prototype$fork)
     this.task = task
     this.next = next
   }
-  fork(succeed/*:(value:a) => void*/, fail/*:(error:x) => void*/)/*:void*/ {
+  fork(succeed/*:(value:b) => void*/, fail/*:(error:x) => void*/)/*:void*/ {
     this.task.fork
     ( value => this.next(value).fork(succeed, fail)
     , fail
     )
   }
 }
+const Chain$prototype$fork = Chain.prototype.fork
 
-class Map /*::<x, a, b>*/ extends Task/*::<x, b>*/ {
+class Map /*::<x, a, b>*/ extends Chain/*::<x, a, b>*/ {
   /*::
-  task: Task<x, a>;
-  f: (input:a) => b;
+  mapper: (input:a) => b;
   */
-  constructor(task/*:Task<x, a>*/, f/*:(input:a) => b*/) {
-    super(Map.prototype.fork)
+  constructor(task/*:Task<x, a>*/, mapper/*:(input:a) => b*/) {
+    // Note: Had to trick flow into thinking that `Format.prototype.handle` was
+    // passed, otherwise it fails to infer polymorphic nature.
+    super(task, (Map$prototype$next/*::, Map.prototype.next*/))
     this.task = task
-    this.f = f
+    this.mapper = mapper
   }
-  fork(succeed/*:(value:b) => void*/, fail/*:(error:x) => void*/)/*:void*/ {
-    this.task.fork
-    ( value => succeed(this.f(value))
-    , fail
-    )
+  next(input/*:a*/)/*:Task<x, b>*/ {
+    return new Succeed(this.mapper(input))
   }
 }
+const Map$prototype$next = Map.prototype.next
 
 class Capture/*::<x, y, a>*/extends Task/*::<y, a>*/{
   /*::
@@ -197,7 +205,7 @@ class Capture/*::<x, y, a>*/extends Task/*::<y, a>*/{
   handle: (error:x) => Task<y, a>;
   */
   constructor(task/*:Task<x, a>*/, handle/*:(error:x) => Task<y, a>*/) {
-    super(Capture.prototype.fork)
+    super(Capture$prototype$fork)
     this.task = task
     this.handle = handle
   }
@@ -208,225 +216,159 @@ class Capture/*::<x, y, a>*/extends Task/*::<y, a>*/{
     )
   }
 }
+const Capture$prototype$fork = Capture.prototype.fork
 
-class Format/*::<x, y, a>*/extends Task/*::<y, a>*/{
+class Format/*::<x, y, a>*/extends Capture/*::<x, y, a>*/{
   /*::
-  task: Task<x, a>;
-  f: (input:x) => y;
+  formatter: (error:x) => y;
   */
-  constructor(task/*:Task<x, a>*/, f/*:(input:x) => y*/) {
-    super(Format.prototype.fork)
+  constructor(task/*:Task<x, a>*/, formatter/*:(error:x) => y*/) {
+    // Note: Had to trick flow into thinking that `Format.prototype.handle` was
+    // passed, otherwise it fails to infer polymorphic nature.
+    super(task, (Format$prototype$handle/*::,Format.prototype.handle*/))
     this.task = task
-    this.f = f
+    this.formatter = formatter
   }
-  fork(succeed/*:(value:a) => void*/, fail/*:(error:y) => void*/)/*:void*/ {
-    this.task.fork
-    ( succeed
-    , error => fail(this.f(error))
-    )
+  handle(error/*:x*/)/*:Task<y, a>*/ {
+    return new Fail(this.formatter(error))
   }
 }
+const Format$prototype$handle = Format.prototype.handle
 
-class Deferred /*::<x, a>*/ extends Task /*::<x, a>*/ {
-  /*::
-  result: ?Task<x, a>;
-  */
-  constructor() {
-    super(Deferred.prototype.fork)
-  }
-  resume(task/*:Task<x, a>*/)/*:void*/ {
-    if (this.result != null) {
-      throw Error('Deferred task can only be resumed once')
-    }
-    this.result = task
-  }
-  fork(succeed/*:(a:a) => void*/, fail/*:(x:x) => void*/)/*:void*/ {
-    throw Error('Forking defferred task is not allowed')
-  }
-}
 
 const noop = () => void(0)
 
-class Running /*::<x, a>*/ {
+window.reflex$Process$$nextID =
+  ( window.reflex$Process$$nextID == null
+  ? 0
+  : window.reflex$Process$$nextID
+  );
+
+
+class Process /*::<message, reason>*/ {
   /*::
-  task: Task<x, a>;
+  id: ProcessID;
+  root: Task<any, any>;
+  stack: Array<Capture<*, *, *> | Chain<*, *, *>>;
+  position: number;
+  mailbox: Array<message>;
+  abort: ?Abort<reason>;
+  exit: ?reason;
+  isActive: boolean;
   */
-  constructor /*::<x, a>*/(task/*:Task<x, a>*/) {
-    this.task = task
-  }
-}
-
-class Done /*::<x, a>*/ {
-  /*::
-  task: Task<x, a>;
-  */
-  constructor(task/*:Succeed<x, a> | Fail<x, a>*/) {
-    this.task = task
-  }
-}
-
-class Blocked /*::<x, a>*/ {
-  /*::
-  task: Task<x, a>;
-  */
-  constructor(task/*:Task<x, a>*/) {
-    this.task = task
-  }
-}
-
-/*::
-type Routine <x, a>
-  = Blocked <x, a>
-  | Done <x, a>
-  | Running <x, a>
-*/
-
-const run = /*::<x, a>*/
-  ( root/*:Routine<x, a>*/
-  , succeed/*:(value:a) => void*/
-  , fail/*:(error:x) => void*/
-  )/*:void*/ =>
-  {
-    let routine = new Running(root.task)
-    while (routine instanceof Running) {
-      routine = step(root, routine.task, succeed, fail)
-    }
-
-    if (routine instanceof Blocked) {
-      root.task = routine.task
-    }
-
-    if (routine instanceof Done) {
-      const task = routine.task
-      if (task instanceof Succeed) {
-        succeed(task.value)
-      }
-      else if (task instanceof Fail) {
-        fail(task.error)
-      }
-      else {
-        throw Error('Task end up in an invalid state')
-      }
-    }
-  }
-
-const step = /*::<x, y, a, b>*/
-  ( root/*:Routine*/
-  , task/*:Task<x, a>*/
-  , succeed/*:(value:b) => void*/
-  , fail/*:(error:y) => void*/
-  )/*:Routine*/ =>
-  {
-    if (task instanceof Succeed) {
-      return new Done(task)
-    }
-    else if (task instanceof Fail) {
-      return new Done(task)
-    }
-    else if (task instanceof Deferred) {
-      if (task.result != null) {
-        return new Running(task.result)
-      }
-      else {
-        throw new Error('Blocked routine should not be resumed until blocking task is complete')
-      }
-    }
-    else if (
-      task instanceof Chain ||
-      task instanceof Map ||
-      task instanceof Capture ||
-      task instanceof Format
+  static fork /*::<error, value, message, reason>*/(task/*:Task<error, value>*/, onSucceed/*:(input:value) => void*/, onFail/*:(error:error) => void*/)/*:Process<message, reason>*/ {
+    const process = new Process
+    ( task
+      .map(onSucceed)
+      .format(onFail)
     )
-    {
-      let routine = new Running(task.task)
-      while (routine instanceof Running) {
-        routine = step(root, routine.task, succeed, fail)
+    process.schedule()
+    return process
+  }
+  constructor(task/*:Task<any, any>*/) {
+    this.id = window.reflex$Process$$nextID++;
+    this.position = 0;
+    this.root = task;
+    this.stack = [];
+    this.mailbox = [];
+    this.exit = null;
+  }
+  kill(reason/*:reason*/) {
+    if (this.isActive) {
+      this.isActive = false
+      this.exit = reason
+      if (this.abort) {
+        this.abort(reason)
       }
-
-      if (routine instanceof Done) {
-        const active = routine.task
-
-        if (active instanceof Succeed) {
-          if (task instanceof Chain) {
-            return new Running(task.next(active.value))
-          }
-          else if (task instanceof Map) {
-            return new Done(new Succeed(task.f(active.value)))
-          }
-          else {
-            return routine
-          }
-        }
-        else if (active instanceof Fail) {
-          if (task instanceof Capture) {
-            return new Running(task.handle(active.error))
-          }
-          else if (task instanceof Format) {
-            return new Done(new Fail(task.f(active.error)))
-          }
-          else {
-            return routine
-          }
-        }
-        else {
-          return routine
-        }
-      }
-      else if (routine instanceof Blocked) {
-        if (task instanceof Chain) {
-          return new Blocked(new Chain(routine.task, task.next))
-        }
-        else if (task instanceof Capture) {
-          return new Blocked(new Capture(routine.task, task.handle))
-        }
-        else if (task instanceof Map) {
-          return new Blocked(new Map(routine.task, task.f))
-        }
-        else if (task instanceof Format) {
-          return new Blocked(new Format(routine.task, task.f))
-        }
-        else {
-          return routine
-        }
-      }
-      else {
-        return routine
-      }
-    }
-    else {
-      let isBlocked = false
-      let isResumed = false
-      const deferred = new Deferred()
-
-      task.fork
-      ( value => {
-          if (!isResumed) {
-            isResumed = true
-            deferred.resume(new Succeed(value))
-            if (isBlocked) {
-              run(root, succeed, fail)
-            }
-          }
-        }
-      , error => {
-          if (!isResumed) {
-            isResumed = true
-            deferred.resume(new Fail(error))
-            if (isBlocked) {
-              run(root, succeed, fail)
-            }
-          }
-        }
-      )
-
-      isBlocked = !isResumed
-
-      const routine =
-        ( deferred.result != null
-        ? new Running(deferred.result)
-        : new Blocked(deferred)
-        )
-
-      return routine
     }
   }
+  schedule() {
+    this.step()
+  }
+  step() {
+    const process = this
+    while (process.isActive) {
+      const task = process.root
+      if (task instanceof Succeed) {
+        // If task succeeded skip all the error handling.
+        while
+        ( process.position < process.stack.length &&
+          process.stack[process.position] instanceof Capture
+        )
+        {
+          process.position ++
+        }
+
+        // If end of the stack is reached then break
+        if (process.position >= process.stack.length) {
+          break
+        }
+
+        // Otherwise step into next task.
+        const chain = process.stack[process.position++]
+        /*:: if (chain instanceof Chain) */
+        process.root = chain.next(task.value)
+        continue;
+      }
+
+      if (task instanceof Fail) {
+        // If task fails skip all the chaining.
+        while
+        ( process.position < process.stack.length &&
+          process.stack[process.position] instanceof Chain
+        )
+        {
+          process.position ++
+        }
+
+        // If end of the stack is reached then break.
+        if (this.position >= process.stack.length) {
+          break
+        }
+
+        // Otherwise step into next task.
+        const capture = process.stack[process.position++]
+        /*:: if (capture instanceof Capture) */
+        process.root = capture.handle(task.error)
+        continue
+      }
+
+      if (task instanceof Chain) {
+        process.stack.splice(0, process.position, task)
+        process.root = task.task
+        process.position = 0
+        continue
+      }
+
+      if (task instanceof Capture) {
+        process.stack.splice(0, process.position, task)
+        process.root = task.task
+        process.position = 0
+        continue
+      }
+
+      if (task instanceof Task) {
+        let ended = false
+        process.abort = task.execute
+        ( value => {
+            if (!ended) {
+              ended = true
+              process.root = new Succeed(value)
+              process.abort = null
+              process.schedule()
+            }
+          }
+        , error => {
+            if (!ended) {
+              ended = true
+              process.root = new Fail(error)
+              process.abort = null
+              process.schedule()
+            }
+          }
+        );
+        break;
+      }
+    }
+  }
+}
