mozilla/reflex

commit 4e13b24ebc4936c1cc3a5b47363e8cedac3b32ab

Use catch instead of finally
On spidermonkey use of try/catch is optimized while try/finally is not.

src/signal.js
@@ -84,12 +84,16 @@ class Input /*::<a>*/ {
           const addressBook = this.addressBook
           Input.notify(value, addressBook, 0, addressBook.length)
         }
-      } finally {
+      }
+      catch(error) {
         this.isBlocked = false
         if (this.queue != null && this.queue.length > 0) {
           this.receive(value = this.queue.shift())
         }
+
+        throw error
       }
+      this.isBlocked = false
     }
   }
   subscribe(address/*:Address<a>*/)/*:void*/ {

commit 100486aa726e5ef4a754e6b659e09bda3d2e6f9a

Fix regression introduced by 4e13b24 & ba21bbb
Those changes changed finally blocks to catch blocks which meant they would only be executed on exceptions, which was incorrect since finally blocks always execute & in this instances they performed state updates.

More specifically synchronous tasks would no longer run given that messages would be queued into inbox but new messages were only checked on exceptions or when new messages arrived.

src/dom.js
@@ -29,6 +29,7 @@ class VirtualRoot /*::<model, action>*/ {
     this.address = address
   }
   renderWith(current/*:Driver*/) {
+    let exception = null
     const previous = driver
     driver = current
 
@@ -36,10 +37,14 @@ class VirtualRoot /*::<model, action>*/ {
       driver.render(this.view(this.model, this.address))
     }
     catch(error) {
-      driver = previous
-      throw error
+      exception = error
     }
+
     driver = previous
+
+    if (exception != null) {
+      throw exception
+    }
   }
 }
 VirtualRoot.prototype.$type = "VirtualRoot"

src/preemptive-animation-frame.js
@@ -74,6 +74,7 @@ const dispatchAnimationFrame = /*::<a>*/
   , index/*:number*/
   , time/*:Time*/
   ) => {
+    let exception = null
     const count = requests.length
     try {
       while (index < count) {
@@ -83,9 +84,14 @@ const dispatchAnimationFrame = /*::<a>*/
       }
     }
     catch (error) {
-      if (index < count) {
-        dispatchAnimationFrame(requests, index, time)
-      }
-      throw error
+      exception = error
+    }
+
+    if (index < count) {
+      dispatchAnimationFrame(requests, index, time)
+    }
+
+    if (exception != null) {
+      throw exception
     }
   }

src/signal.js
@@ -29,6 +29,7 @@ class Input /*::<a>*/ {
     }
   }
   static notify(message/*:a*/, addressBook/*:AddressBook<a>*/, from/*:number*/, to/*:number*/)/*:void*/ {
+    let exception = null
     try {
       while (from < to) {
         const address = addressBook[from]
@@ -39,10 +40,15 @@ class Input /*::<a>*/ {
       }
     }
     catch (error) {
-      if (from < to) {
-        Input.notify(message, addressBook, from + 1, to)
-      }
-      throw error
+      exception = error
+    }
+
+    if (from < to) {
+      Input.notify(message, addressBook, from + 1, to)
+    }
+
+    if (exception != null) {
+      throw exception
     }
   }
   static connect(signal/*:Input<a>*/, address/*:Address<a>*/) {
@@ -76,6 +82,7 @@ class Input /*::<a>*/ {
         this.queue.push(value)
       }
     } else {
+      let exception = null
       this.isBlocked = true
       try {
         this.value = value
@@ -85,15 +92,18 @@ class Input /*::<a>*/ {
           Input.notify(value, addressBook, 0, addressBook.length)
         }
       }
-      catch(error) {
-        this.isBlocked = false
-        if (this.queue != null && this.queue.length > 0) {
-          this.receive(value = this.queue.shift())
-        }
-
-        throw error
+      catch (error) {
+        exception = error
       }
+
       this.isBlocked = false
+      if (this.queue != null && this.queue.length > 0) {
+        this.receive(value = this.queue.shift())
+      }
+
+      if (exception != null) {
+        throw exception
+      }
     }
   }
   subscribe(address/*:Address<a>*/)/*:void*/ {